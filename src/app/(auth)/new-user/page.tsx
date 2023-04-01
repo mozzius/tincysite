f7a06f4edb5dc4e621a8ed74dd71a92965d25971
Samuel Newman <mozzius@protonmail.com>
2023-04-02 00:01:45 +0100

    fix vercel deploy

--- src/app/(auth)/new-user/page.tsx
+++ src/app/(auth)/new-user/page.tsx
@@ -7,7 +7,7 @@ export default async function SignInPage() {
   return (
     <section className="flex w-full max-w-sm flex-col items-center justify-center gap-6 rounded bg-white p-8 shadow-2xl">
       <div className="w-full space-y-2 text-center">
-        <h1 className="text-2xl font-bold">Welcome</h1>
+        <h1 className="mt-2 text-2xl font-bold">Welcome</h1>
         <h2 className="text-sm text-gray-500">
           Why don&apos;t you tell us a bit about yourself?
         </h2>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,7 +6,7 @@ export default function SignUpPage() {
   return (
     <section className="flex w-full max-w-sm flex-col items-center justify-center gap-6 rounded bg-white p-8 shadow-2xl">
       <div className="w-full space-y-2 text-center">
-        <h1 className="text-2xl font-bold">Welcome!</h1>
+        <h1 className="mt-2 text-2xl font-bold">Welcome!</h1>
         <h2 className="text-sm text-gray-500">
           If you already have an account,
           <br /> you can{" "}
