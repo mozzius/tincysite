a0803135bbc86e09fcbf1541e5d4e79b64cfe785
Samuel Newman <mozzius@protonmail.com>
2023-04-02 00:33:29 +0100

    change homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Github } from "lucide-react";
 import Link from "next/link";
 
 export default function Homepage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-8">
       <h1 className="text-4xl font-bold">Hello World</h1>
-      <Link href="/sign-in" className="mt-4 rounded border px-2 py-1">
-        Sign in
-      </Link>
+      <div className="flex items-center gap-4">
+        <Link
+          href="/sign-up"
+          className={cn(buttonVariants({ size: "lg", variant: "default" }))}
+        >
+          Sign up
+        </Link>
+        <Link
+          href="https://github.com/mozzius/next13-with-auth"
+          target="_blank"
+          className={cn(
+            buttonVariants({ size: "lg", variant: "outline", hasIcon: true })
+          )}
+        >
+          <Github size={16} />
+          View source
+        </Link>
+      </div>
     </main>
   );
 }
